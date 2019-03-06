4b83279735380ba4ff4a44af77ce0c3f9f076691
mahui <hui.ma@56qq.com>
2019-03-06 17:40:35 +0800

    fix: 修改启动命令

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,5 +1,5 @@
-console.log('-----------------', process.env.NODE_ENV);
-const env = process.env.NODE_ENV.replace(/\s+/,"");
+console.log('-----------------', process.env.INIT_LEVEL);
+const env = process.env.INIT_LEVEL.replace(/\s+/,"");
 const port = env === 'development' ? 3000 : 3389;
 const domain = env === 'development' ? `http://127.0.0.1:${port}`:`http://www.fairyx.cn:${port}`;
 const fileLoadUrl = 'https://fisadmin.56qq.cn/insurance/upload-image.do';
