888b4962aac339e4e03bbd095ea1580b278521c1
mahui <hui.ma@56qq.com>
2019-03-06 17:25:33 +0800

    fix: 修改启动命令

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,5 +1,4 @@
-console.log(process.env);
-console.log('-----------------', process.NODE_ENV);
+console.log('-----------------', process.env.NODE_ENV);
 const env = process.env.NODE_ENV.replace(/\s+/,"");
 const port = env === 'development' ? 3000 : 3389;
 const domain = env === 'development' ? `http://127.0.0.1:${port}`:`http://www.fairyx.cn:${port}`;
