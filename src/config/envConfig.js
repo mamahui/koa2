dcaec28bdb9a7b52f9bb1348a4aeb0d8495cb66e
mahui <hui.ma@56qq.com>
2019-03-07 09:44:37 +0800

    fix: 去除相关信息

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -2,5 +2,4 @@ console.log('-----------------', process.env.NODE_ENV);
 const env = process.env.NODE_ENV.replace(/\s+/,"");
 const port = env === 'development' ? 3000 : 3389;
 const domain = env === 'development' ? `http://127.0.0.1:${port}`:`http://www.fairyx.cn:${port}`;
-const fileLoadUrl = 'https://fisadmin.56qq.cn/insurance/upload-image.do';
-module.exports = {port, domain, env, fileLoadUrl};
+module.exports = {port, domain, env};
