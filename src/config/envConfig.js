0c37b955c9ade9dcdefd0b6eeacd4257005df4a6
mahui <mahui@g7.com.cn>
2020-05-15 17:45:34 +0800

    修改域名端口

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV.replace(/\s+/,"");
-const port = env === 'development' ? 3000 : 3389;
+const port = env === 'development' ? 3000 : 8080;
 const domain = env === 'development' ? `http://127.0.0.1:${port}`:`http://www.fairyx.cn:${port}`;
 
 module.exports = {
